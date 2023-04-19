e5c4d8392dd18b564a9b6b2e1270e5c186f4f3df
Pasi Mikkola <pasimikkola111@gmail.com>
2023-04-19 14:25:43 +0300

    searc added

--- App.js
+++ App.js
@@ -1,30 +1,68 @@
-import React, {useState, useEffect} from 'react';
-import {ImageBackground, StyleSheet, Text, View, Button, TextInput, FlatList, } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './components/HomeScreen';
-import LoginScreen from './components/LoginScreen';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, View, FlatList, Alert } from 'react-native';
+import { Header, Input, Button, Icon,ListItem } from '@rneui/themed';
+import { SearchBar } from '@rneui/themed';
 
-const Stack = createNativeStackNavigator();
+const testList = [
+    {
+        firstName: "Matti",
+        occupation: "Driver"
+    },
+    {
+        firstName: "Paavo",
+        occupation: "Doctor"
+    },
+    {
+        firstName: "Laila",
+        occupation: "Banker"
+    },
+    {
+        firstName: "MaMarkku",
+        occupation: "Banker"
+    },
 
+]
 
-export default function App(){
+export default function App() {
+const [searchedName, setSearchedName] = useState('')
+const [filteredTestList, setFilteredTestList] = useState(testList);
 
-    return (
-        <NavigationContainer>
-        <Stack.Navigator>
-        <Stack.Screen name='Login' options={{headerShown:false,}} component={LoginScreen} />        
-        <Stack.Screen name='Home' options={{headerShown:false,}}component={HomeScreen} />
-        </Stack.Navigator>
-        </NavigationContainer>
-        );
-        
+useEffect(() => {
+    setFilteredTestList(
+      testList.filter(item =>
+        item.firstName.toLowerCase().includes(searchedName.toLowerCase())
+      )
+    );
+  }, [searchedName]);
+
+    return(
+        <View style={styles.container}>
+            <SearchBar
+            containerStyle={{width:'80%'}}
+            value={searchedName}
+            onChangeText={text=>setSearchedName(text)}
+            />
+            <FlatList
+            data={filteredTestList}
+            renderItem={({item}) => 
+            <ListItem>
+                <ListItem.Title>{item.firstName}</ListItem.Title>
+                <ListItem.Subtitle>{item.occupation}</ListItem.Subtitle>
+            </ListItem>}       
+            />
+        </View>
+    )
 
 };
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      alignItems: 'center',
+     marginTop: 50,
+     flex: 1,
+     backgroundColor: '#fff',
+    justifyContent:'center',
+     alignItems:'center',
+     flexDirection:'column',
     },
-});
+    
+   });
---
+++ AppLoginandRegister.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import {ImageBackground, StyleSheet, Text, View, Button, TextInput, FlatList, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './components/HomeScreen';
+import LoginScreen from './components/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+
+export default function App(){
+
+    return (
+        <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name='Login' options={{headerShown:false,}} component={LoginScreen} />        
+        <Stack.Screen name='Home' options={{headerShown:false,}}component={HomeScreen} />
+        </Stack.Navigator>
+        </NavigationContainer>
+        );
+        
+
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+    },
+});
